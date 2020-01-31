8663d9d6082a154a053259227c0d1a38189d67d8
satres <trasmussen3@wisc.edu>
2020-01-31 15:27:41 -0600

    First change add image

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Image image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
